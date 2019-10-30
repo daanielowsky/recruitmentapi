afd6e769527dcb12a75a2453c09bc552f066e717
daanielowsky <pawlicki.daniel@wp.pl>
2019-10-30 14:32:51 +0100

    Initialized Spring API

---
+++ src/main/java/com/github/daanielowsky/RecruitmentAssistant/RecruitmentAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.github.daanielowsky.RecruitmentAssistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/daanielowsky/RecruitmentAssistant/RecruitmentAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.daanielowsky.RecruitmentAssistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
