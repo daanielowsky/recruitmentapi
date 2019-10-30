9ae09d8e7bd5eb078cee0ad66560caac3863fca4
daanielowsky <pawlicki.daniel@wp.pl>
2019-10-30 15:55:43 +0100

    Added Security Config

---
+++ src/main/java/com/github/daanielowsky/RecruitmentAssistant/config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.github.daanielowsky.RecruitmentAssistant.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private DataSource dataSource;
+
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("select username, password, true from user where username = ?")
+                .authoritiesByUsernameQuery("select username, 'ROLE_USER' from user where username = ?");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/register").permitAll()
+                .antMatchers("/login").anonymous()
+                .antMatchers("/recruitment/*").authenticated()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/recruitment")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .and()
+                .csrf()
+                .disable()
+                .rememberMe()
+                .key("recruitment")
+                .rememberMeParameter("remember-me")
+                .tokenValiditySeconds(7*24*60*60);
+    }
+}
